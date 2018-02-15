3f53c95da01d5be3341ac6e67512173d53b779d7
Diego <diegosampaiorj@gmail.com>
2018-02-15 21:04:59 -0200

    jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,30 @@
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamnhoFrase").text();
+var tamanhoFrase = $("#tamnho-frase");
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function(){
+    var cronometroID = setInterval(function(){
+        tempoRestante --;
+        $("#tempo-digitacao").text(tempoRestante); 
+        
+        if(tempoRestante < 1){
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+        
+    },1000);
+});
